963e07cdcf1479d814182b6a39b3a35279e8c4dd
redcontroller <jasinis102@gmail.com>
2024-10-07 18:00:16 +0900

    fix: 폴더 경로 수정

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import wenivImg from '../img/weniv_logo.png';
 
 const Container = styled.footer`
   width: 100%;
@@ -14,7 +13,7 @@ const Img = styled.img`
 
 const P = styled.p`
   width: fit-content;
-  color: rgba(255, 255, 255, 0.70);
+  color: rgba(255, 255, 255, 0.7);
   font-size: 0.9rem;
   font-family: 'Noto Sans KR';
   margin: 0 auto 3.2rem;
@@ -24,8 +23,11 @@ const P = styled.p`
 export default function Footer() {
   return (
     <Container>
-        <Img src={wenivImg} alt='위니브 로고' />
-        <P>※ 본 서비스 내 이미지 및 콘텐츠의 저작권은 주식회사 WeNiv에 있습니다.<br/> 수정 및 재배포, 무단 도용 시 법적인 문제가 발생할 수 있습니다.</P>
+      <Img src="/img/weniv_logo.png" alt="위니브 로고" />
+      <P>
+        ※ 본 서비스 내 이미지 및 콘텐츠의 저작권은 주식회사 WeNiv에 있습니다.
+        <br /> 수정 및 재배포, 무단 도용 시 법적인 문제가 발생할 수 있습니다.
+      </P>
     </Container>
-  )
+  );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import styled, {keyframes} from 'styled-components';
-import logo from '../img/logo.png';
-import back from '../img/logo-back.png';
+import styled, { keyframes } from 'styled-components';
 
 const Container = styled.header`
-    width: 100%;
-    height: fit-content;
-    /* background-color: greenyellow; */
-    `;
+  width: 100%;
+  height: fit-content;
+`;
 const move = keyframes`
     0% {
         transform: rotate(0deg);
@@ -18,69 +15,67 @@ const move = keyframes`
     `;
 const H1 = styled.h1``;
 const Logo = styled.div`
-    width: 60rem;
-    margin: 0 auto;
-    padding: 5rem 0 5rem 0;
-    position: relative;
-    /* box-sizing: border-box; */
-    /* background-color: palegreen; */
+  width: 60rem;
+  margin: 0 auto;
+  padding: 5rem 0 5rem 0;
+  position: relative;
 
-    &::after {
-        content: "";
-        display: inline-block;
-        /* width: 100%; */
-        width: 20vw;
-        max-width: 20rem;
-        background-image: url(${back});
-        background-size: contain;
-        background-position: center;
-        background-repeat: no-repeat;
-        animation: ${move} 15s linear infinite;
-        aspect-ratio: 1/1;
-        
-        position: absolute;
-        top: 5%;
-        left: 35%;
-        /* left: 35%; */
-        /* left: calc(50% - 10vw); */
-    }
+  &::after {
+    content: '';
+    display: inline-block;
+    width: 20vw;
+    max-width: 20rem;
+    background-image: url('/img/logo-back.png');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    animation: ${move} 15s linear infinite;
+    aspect-ratio: 1/1;
+
+    position: absolute;
+    top: 5%;
+    left: 35%;
+  }
 `;
 const ImgLogo = styled.img`
-    display: block;
-    width: 60vw;
-    max-width: 60rem;
-    margin: 0 auto;
+  display: block;
+  width: 60vw;
+  max-width: 60rem;
+  margin: 0 auto;
 `;
 const Q = styled.q`
-    display: block;
-    width: fit-content;
-    color: #FF7499;
-    font-family: 'OTEnjoystoriesBA';
-    font-size: 2.3rem;
-    font-style: normal;
-    margin: 0 auto 4.4rem;
+  display: block;
+  width: fit-content;
+  color: #ff7499;
+  font-family: 'OTEnjoystoriesBA';
+  font-size: 2.3rem;
+  font-style: normal;
+  margin: 0 auto 4.4rem;
 `;
 const P = styled.p`
-    font-size: 2rem;
-    font-weight: 500;
-    text-align: center;
+  font-size: 2rem;
+  font-weight: 500;
+  text-align: center;
 `;
 const Strong = styled.strong`
-    font-size: 2.2rem;
+  font-size: 2.2rem;
 `;
 
 export default function Header() {
   return (
     <Container>
-        <H1 className='a11y-hidden'>1만 시간의 법칙 계산기</H1>
-        <Logo>
-            <ImgLogo src={logo} alt="페이지 로고" />
-        </Logo>
-        <Q>연습은 어제의 당신보다 당신을 더 낫게 만든다.</Q>
-        <P><Strong>1만 시간의 법칙은</Strong><br/>
-          어떤 분야의 전문가가 되기 위해서는<br/>
-          최소한 1만 시간의 훈련이 필요하다는 법칙이다.
-        </P>
+      <H1 className="a11y-hidden">1만 시간의 법칙 계산기</H1>
+      <Logo>
+        <ImgLogo src="/img/logo.png" alt="페이지 로고" />
+      </Logo>
+      <Q>연습은 어제의 당신보다 당신을 더 낫게 만든다.</Q>
+      <P>
+        <Strong>1만 시간의 법칙은</Strong>
+        <br />
+        어떤 분야의 전문가가 되기 위해서는
+        <br />
+        최소한 1만 시간의 훈련이 필요하다는 법칙이다.
+      </P>
     </Container>
-  )
+  );
 }
